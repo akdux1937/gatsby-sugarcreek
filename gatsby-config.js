cd0b20f36e80b710ba92f09dd4f2191597e29ba8
andrew krcatovich <andrew.krcatovich@gmail.com>
2020-09-09 16:10:25 -0500

    Added FAQ, Protocols and How to purchase pages to Netlify

--- gatsby-config.js
+++ gatsby-config.js
@@ -80,13 +80,13 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `Foundation`,
-        short_name: `Foundation`,
+        name: `Sugar Creek Farms`,
+        short_name: `Sugar Creek Farms`,
         start_url: `/`,
         background_color: `#f7f0eb`,
-        theme_color: `#a2466c`,
+        theme_color: `#004e36`,
         display: `standalone`,
-        icon: `static/assets/stackrole.png`,
+        icon: `static/assets/scf-logo-green.svg`,
       },
     },
     'gatsby-plugin-offline',
