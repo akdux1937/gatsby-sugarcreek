6dac9739e976e496bf8a8cde92b05c9aabb16275
andrew krcatovich <andrew.krcatovich@gmail.com>
2020-09-09 15:35:03 -0500

    Changed some styling

--- src/components/logo.js
+++ src/components/logo.js
@@ -1,9 +1,11 @@
 import React from "react"
 import { Link } from "gatsby"
+import logo from '../../static/assets/scf-logo-green.svg'
 
 const Logo = (props) => (
   <div className="site-logo">
-    <Link to="/">{props.title}</Link>
+    <Link to="/"><img className="svg" src={logo} alt="Sugar Creek Farms" /></Link>
+    {/* <Link to="/">{props.title}</Link> */}
   </div>
 )
 
